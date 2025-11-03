1748b86662fc370d512fbcebc7f0421338cd6f7d
Yanak1 <1834397877@qq.com>
2025-11-03 20:19:22 +0800

    feat: add the ability to use multiple crystal for player

--- Assets/scrips/Skills/CrystalSkill.cs
+++ Assets/scrips/Skills/CrystalSkill.cs
@@ -15,9 +15,20 @@ public class CrystalSkill : Skill
     [SerializeField] private bool canMoveToEnemy;
     [SerializeField] private float moveSpeed;
 
+    [Header("Multi stacking crystal")]
+    [SerializeField] private bool canUseMultiStacks;
+    [SerializeField] private int amountOfStacks;
+    [SerializeField] private float multiStakeCooldown;
+    [SerializeField] private List<GameObject> crystalList = new List<GameObject>();
+
     public override void UseSkiil()
     {
         base.UseSkiil();
+
+        if (TryUseMultiCrystal())
+        {
+            return;
+        }
 
         if (currentCrystal == null)
         {
@@ -37,6 +48,41 @@ public class CrystalSkill : Skill
             player.transform.position = currentCrystal.transform.position;
             currentCrystal.transform.position = playerPos;
             currentCrystal.GetComponent<CrystalSkillController>()?.crystalCompleted();
+        }
+    }
+
+
+
+    private bool TryUseMultiCrystal()
+    {
+        if (canUseMultiStacks)
+        {
+            if (crystalList.Count > 0)
+            {
+                cooldown = 0;
+                GameObject crystalToSpawn = crystalList[crystalList.Count - 1];
+                GameObject newCrystal = Instantiate(crystalToSpawn, player.transform.position, Quaternion.identity);
+
+                crystalList.Remove(crystalToSpawn);
+
+                newCrystal.GetComponent <CrystalSkillController>().SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed);
+
+                if (crystalList.Count <= 0)
+                {
+                    cooldown = multiStakeCooldown;
+                    RefillCrystal();
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void RefillCrystal()
+    {
+        for (int i = 0; i < amountOfStacks; i++)
+        {
+            crystalList.Add(crystalPrefab);
         }
     }
 }
